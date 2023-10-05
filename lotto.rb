b2c85c6390983fb508db56e54ac16de055a488bb
Hilmi AKKAYA <141329888+hilmiakkaya1@users.noreply.github.com>
2023-10-05 16:27:39 +0300

    Raise Calculation Program

--- lotto.rb
+++ lotto.rb
@@ -1,6 +1,31 @@
-lotto_numbers = (1..49).to_a.sample(6)
+maas = 0
 
-lotto_numbers.sort!
+while maas <= 0
+  puts "Lütfen maaşınızı giriniz"
+  maas = gets.chomp.to_i
 
-puts "Sizin için seçilen rastgele 6 adet sayı:"
-puts lotto_numbers.join(', ')
+  if maas <= 0
+    puts "Geçerli bir maaş giriniz."
+  end
+end
+
+yuzde = nil
+
+while yuzde.nil? || yuzde.to_i.to_s != yuzde
+  puts "Lütfen zam yüzdesini giriniz (örneğin, %35 ise 35 olarak giriniz)"
+  yuzde = gets.chomp
+
+  if yuzde.to_i.to_s != yuzde
+    puts "Geçerli bir rakam giriniz."
+  end
+end
+
+yuzde = yuzde.to_i
+
+
+zam_miktari = (maas * yuzde) / 100
+zam_sonrasi_maas = maas + zam_miktari
+
+
+puts "Zam miktarı: #{zam_miktari} TL"
+puts "Zamdan sonra maaşınız: #{zam_sonrasi_maas} TL"
